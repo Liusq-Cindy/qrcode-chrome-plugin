ac79520b770916605324d06893d1381cb7b17f08
lsq <306749392@qq.com>
2019-11-01 16:19:52 +0800

    feat: 一个chrome生成二维码的小插件

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+﻿// 清除网址
+$('#clear-btn').click(function() {
+  // 清除输入的内容
+  $('#textstr').val('');
+  jQuery(function(){
+    // 清空原有二维码
+    $('#output').empty();
+  })
+})
+// 转成二维码
+$('#code-btn').click(function() {
+  let str = $('#textstr').val() || '';
+  jQuery(function(){
+    // 清空原有二维码，生成新的二维码
+    $('#output').empty();
+    jQuery('#output').qrcode({
+      render: "canvas", //也可以替换为table
+      width: 150,
+      height: 150,
+      text: str
+    });
+  })
+})
